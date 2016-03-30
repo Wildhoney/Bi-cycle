4a58de85a69b0d440f9a6c431482c78971d32042
Wildhoney <adam.timberlake@gmail.com>
2016-03-30 20:07:34 +0100

    Added working tests

--- tests/bicycle.test.js
+++ tests/bicycle.test.js
@@ -1,9 +1,15 @@
 import test from 'ava';
-import { cycle } from '../src/bicycle';
+import { cycle, NEXT } from '../src/bicycle';
 
-test('able to increment and decrement;', t => {
-
+test('able to increment and decrement with default parameters;', t => {
     const c = cycle();
-    console.log(c);
+    t.is(c.next(NEXT).value, 1);
+    t.is(c.next(NEXT).value, 2);
+    t.is(c.next(NEXT).value, 3);
+});
 
+test('able to return the current index when no arguments passed;', t => {
+   const c = cycle();
+    t.is(c.next(NEXT).value, 1);
+    t.is(c.next().value, 1);
 });
