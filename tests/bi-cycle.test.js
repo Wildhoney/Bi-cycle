3c48373a0a66ac1e9fadcd3dc98c1f8e4628d21b
Wildhoney <adam.timberlake@gmail.com>
2016-04-08 18:28:54 +0100

    Fixed broken tests

--- tests/bi-cycle.test.js
+++ tests/bi-cycle.test.js
@@ -21,7 +21,8 @@ test('able to traverse the index using all instructions;', t => {
     t.is(next(), 1);
     t.is(next(), 2);
     t.is(last(), Infinity);
-    t.is(first(), 0);
+    t.is(first(), -Infinity);
+    t.is(set(0), 0);
     t.is(next(), 1);
     t.is(set(5), 5);
     t.is(current(), 5);
@@ -32,10 +33,10 @@ test('able to add a maximum limit on the index;', t => {
     const { first, previous, next, last, set } = Bicycle({ start: 0, max: 5 });
     t.is(next(), 1);
     t.is(set(5), 5);
-    t.is(next(), 0);
+    t.is(next(), -Infinity);
     t.is(last(), 5);
     t.is(previous(), 4);
-    t.is(first(), 0);
+    t.is(first(), -Infinity);
 });
 
 test('able to specify the starting index;', t => {
@@ -48,8 +49,24 @@ test('able to specify partial options;', t => {
     t.is(next(), 1);
 });
 
+test('able to set the min and max properties;', t => {
+    const { next, previous } = Bicycle({ min: -2, max: 3 });
+    t.is(next(), 1);
+    t.is(next(), 2);
+    t.is(next(), 3);
+    t.is(next(), -2);
+    t.is(next(), -1);
+    t.is(next(), 0);
+    t.is(next(), 1);
+    t.is(previous(), 0);
+    t.is(previous(), -1);
+    t.is(previous(), -2);
+    t.is(previous(), 3);
+    t.is(previous(), 2);
+});
+
 test('able to change to a finite list so index does not reset;', t => {
-    const { previous, next, set } = Bicycle({ max: 5, infinite: false });
+    const { previous, next, set } = Bicycle({ min: 0, max: 5, infinite: false });
     t.is(next(), 1);
     t.is(next(), 2);
     t.is(set(0), 0);
